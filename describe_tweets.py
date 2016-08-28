30efc8f23486f3aaef76348537d4898f16276b94
skeydan <keydana@gmx.de>
2016-08-28 20:14:20 +0200

    add co-occurrences

--- describe_tweets.py
+++ describe_tweets.py
@@ -1,13 +1,15 @@
 import pandas as pd
 import numpy as np
 
+import config
+import operator
 from nltk.corpus import stopwords
-
+from collections import defaultdict
 from sklearn.feature_extraction.text import CountVectorizer
 
 
-tokenized_file = 'data/tweets_tokenized.csv'
-english_file = 'data/tweets_english.csv'
+tokenized_file = config.data_dir  + '/' + 'tweets_tokenized.csv'
+english_file = config.data_dir  + '/' + 'tweets_english.csv'
 
 alltweets_tokenized = pd.read_csv(tokenized_file, encoding='utf-8',
                                   usecols = ['id_str', 'user_id', 'created_at', 'lang', 'text', 'favorite_count', 'entities',
@@ -19,7 +21,7 @@ tweets_english = pd.read_csv(english_file, encoding='utf-8',
                                          'retweet_count', 'quoted_status_id_str', 'text_tokenized'])
 
 tweets_by_language = pd.groupby(alltweets_tokenized, 'lang').size().sort_values(ascending = False)
-#print tweets_by_language
+#print(tweets_by_language)
 
 X_train = tweets_english['text_tokenized']
 X_train = X_train.apply(lambda t: t.replace(',',''))
@@ -27,43 +29,86 @@ stopwords_nltk = set(stopwords.words("english"))
 relevant_words = set(['not', 'nor', 'no', 'wasn', 'ain', 'aren', 'very', 'only', 'but', 'don', 'isn', 'weren'])
 stopwords_filtered = list(stopwords_nltk.difference(relevant_words))
 
-print '''
+
+print( '''
 /******************************************************************************
 *    Inspect vocabularies built by CountVectorizer for ngram ranges 1,2,3,4,5     *
 ******************************************************************************/
+''')
 '''
-
-for remove_stop_words in [stopwords_filtered]:
-#for remove_stop_words in [stopwords_filtered, None]:
-    print '\n\nStop words removed: {}\n*******************************'.format(remove_stop_words)    
-    for i in range(1):
-    #for i in range(1,6):
+for remove_stop_words in [stopwords_filtered, None]:
+    print('\n\nStop words removed: {}\n*******************************'.format(remove_stop_words))
+    for i in range(1,6):
         vectorizer = CountVectorizer(analyzer = "word", tokenizer = None, preprocessor = None, token_pattern = '\S+',
-                                    stop_words = remove_stop_words, max_features = 10000, ngram_range = (1,i+1))
+                                    stop_words = remove_stop_words, max_features = 100000, ngram_range = (1,i))
 
         # sparse matrix
-        words_array = vectorizer.fit_transform(X_train).toarray()
-
+        words_matrix = vectorizer.fit_transform(X_train)
         vocabulary = vectorizer.get_feature_names()
-        #print vocabulary[0:10]
-        #print vectorizer.vocabulary_.get('able')
-
-        counts = np.sum(words_array, axis=0)
+        counts = words_matrix.sum(axis=0).A1
+       
         word_counts_overall = pd.DataFrame({'word': vocabulary, 'count': counts})
            
-        word_counts_for_max_ngram = word_counts_overall[word_counts_overall.word.apply(lambda c: len(c.split()) >= i+1)]
+        word_counts_for_max_ngram = word_counts_overall[word_counts_overall.word.apply(lambda c: len(c.split()) >= i)]
            
         word_counts_for_max_ngram_sorted = word_counts_for_max_ngram.sort_values(by='count', ascending=False)
-        print '\nMost frequent ngrams for ngrams in range 1 - {}:'.format(i)
+        print('\nMost frequent ngrams for ngrams in range 1 - {}:'.format(i))
             
-        print word_counts_for_max_ngram_sorted[:100]
+        print(word_counts_for_max_ngram_sorted[:100])
         
-        
-print '''
+'''        
+print( '''
 /******************************************************************************
-*    Co-Occurrence Matrix     *
+*    Co-Occurrence Matrix                                                     *
 ******************************************************************************/
-'''
+''')
+
+def get_co_occurrences(record):
+    wordlist = record.split()
+    for i in range(len(wordlist) - 1):
+        for j in range(i+1, len(wordlist)):    
+            w1, w2 = sorted([wordlist[i], wordlist[j]]) 
+            if w1 != w2:
+                co_occurrences[w1][w2] += 1
+                co_occurrences[w2][w1] += 1
+                
+
+X_train = X_train[:2]
+X_train =pd.Series(['one two three', 'one two', 'two three three three'])
+
+# co_occurrences, by word, ordered alphabetically
+# each word appears as key
+co_occurrences = defaultdict(lambda : defaultdict(int))
+X_train.apply(get_co_occurrences) 
+print('co_occurrences:')
+
+i=0
+for word in co_occurrences:
+    i = i+1
+    if i > 9: break
+    print (word)
+    for oc in co_occurrences[word]:
+        print (oc,':',co_occurrences[word][oc])
+        
+co_occurrences_df = pd.DataFrame.from_dict(co_occurrences)   
+print(co_occurrences_df[:10])
+
+# co_occurrences, for each key ordered by frequency
+# type(co_occurrences_by_freq['one'])
+co_occurrences_by_freq = {word: sorted(co_occurrences[word].items(), key = lambda k_v: v[1], reverse=True) for word in co_occurrences}
+print('co_occurrences ordered by frequency for each word:')
+
+i=0
+for word in co_occurrences_by_freq:
+    i = i+1
+    if i > 9: break
+    print (word)
+    for tup in co_occurrences_by_freq[word]: print(tup[0] + ': ' + str(tup[1]))
+
+# co_occurrences sorted by highest value per word
+co_occurrences_by_highest_freq = sorted(co_occurrences_by_freq.items(), key = lambda k_v: k_v[1][0][1], reverse=True)
+print('co_occurrences sorted by highest value per word:')
+print(co_occurrences_by_highest_freq[:10])
 
 topics = ['oracle', 'windows', 'cloud', 'sql', 'orclapex', ]
 topics_hashtags = map(lambda s: '#'+s, topics)
@@ -81,17 +126,18 @@ sentiment_words = ['great', 'good', 'like', 'thanks']
  
 
 
-print '''
+
+print( '''
 /******************************************************************************
 *    Sentiment Analysis nltk                                                  *
 ******************************************************************************/
-'''
+''')
 
 
-print '''
+print( '''
 /******************************************************************************
 *    Sentiment Analysis unsupervised                                          *
 ******************************************************************************/
-'''
+''')
 
 
