7b0b72af89837d8e406bfadfc5f079745c481f81
skeydan <keydana@gmx.de>
2016-10-17 10:28:41 +0200

    only save specific fields

--- twitter_stream_listener.py
+++ twitter_stream_listener.py
@@ -9,6 +9,7 @@ import config
 import json
 import datetime
 import os
+import csv
 
 class UserIdListener(StreamListener):
     def __init__(self, target):      
@@ -27,7 +28,12 @@ class UserIdListener(StreamListener):
             if not os.path.exists(daily_dir): os.makedirs(daily_dir)
             user_file = '/'.join([daily_dir, "user_" + user_id + "_" + curdate]) 
             with open(user_file, 'a') as f:
-                f.write(data)
+                writer = csv.writer(f)
+                writer.writerow(
+                        [tweet.id_str, tweet.user.id, tweet.created_at, tweet.lang, tweet.text, tweet.retweeted, tweet.favorite_count, tweet.entities,
+                        tweet.in_reply_to_screen_name, tweet.in_reply_to_status_id_str, tweet.in_reply_to_user_id, 
+                        tweet.quoted_status_id_str if hasattr(tweet, 'tweet.quoted_status_id_str') else None,
+                        tweet.retweet_count])
                 return True
         except BaseException as e:
             print("Error on_data: %s" % str(e))
@@ -43,5 +49,6 @@ if __name__ == '__main__':
     auth.set_access_token(config.access_token, config.access_secret)
 
     stream = Stream(auth, l)
-    stream.filter(follow = l.user_ids)
-    #stream.filter(track='machine learning')
+    # https://dev.twitter.com/streaming/overview/request-parameters#follow
+    #stream.filter(follow = l.user_ids)
+    stream.filter(track = 'machinelearning')
